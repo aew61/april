c6648df4eb57d1b0c776f4b85e76f8e269eeff33
aew61 <aew61@case.edu>
2017-10-31 23:54:12 -0400

    initial directory structure

---
+++ include/april/parser/parser.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef APRIL_PARSER_PARSER_H
+#define APRIL_PARSER_PARSER_H
+
+
+// SYSTEM INCLUDES
+
+
+// C PROJECT INCLUDES
+
+
+int add(int a, int b);
+
+#endif
+
---
+++ src/parser/parser.c
@@ -0,0 +1,12 @@
+// SYSTEM INCLUDES
+
+
+// C PROJECT INCLUDES
+#include "april/parser/parser.h"
+
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
